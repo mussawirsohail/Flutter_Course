eadab76b48bc6203ba4346cb7bda237065b983be
Mussawir Sohail <159765711+mussawirsohail@users.noreply.github.com>
2025-04-14 15:46:13 +0500

    Create simple-calculator.dart

---
+++ simple-calculator.dart
@@ -0,0 +1,17 @@
+void main() {
+  double num1 = 10;
+  double num2 = 5;
+
+  double sum = num1 + num2;
+  double difference = num1 - num2;
+  double product = num1 * num2;
+  double quotient = num1 / num2;
+
+  print('=== Simple Calculator ===');
+  print('Number 1: $num1');
+  print('Number 2: $num2');
+  print('Sum         : $sum');
+  print('Difference  : $difference');
+  print('Product     : $product');
+  print('Quotient    : $quotient');
+}
